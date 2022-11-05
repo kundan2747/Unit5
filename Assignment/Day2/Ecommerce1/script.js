2cc5f8afe0394e6168868ee61bcfb7b12e290c7a
kundan2747 <kundanupadhayay25@gmail.com>
2022-11-05 22:42:28 +0530

    Ecommerce-1 added

---
+++ Assignment/Day2/Ecommerce1/script.js
@@ -0,0 +1,24 @@
+document.querySelector("form").addEventListener("submit", addItem);
+var productsArr = JSON.parse(localStorage.getItem("Products")) || [];
+
+function CreateItem(name, category, image, price, gender, sold) {
+  this.name = name;
+  this.category = category;
+  this.image = image;
+  this.price = price;
+  this.gender = gender;
+  this.sold = sold;
+}
+
+function addItem() {
+  event.preventDefault();
+  var name = document.getElementById("name").value;
+  var category = document.getElementById("category").value;
+  var image = document.getElementById("image").value;
+  var price = document.getElementById("price").value;
+  var gender = document.getElementById("gender").value;
+  var sold = document.getElementById("sold").value;
+  var item = new CreateItem(name, category, image, price, gender, sold);
+  productsArr.push(item);
+  localStorage.setItem("Products", JSON.stringify(productsArr));
+}
