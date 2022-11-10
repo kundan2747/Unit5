ea279afab8d092f962900c1607a43f939cb82a48
kundan2747 <kundanupadhayay25@gmail.com>
2022-11-10 23:52:47 +0530

    sorting feature added

--- Assignment/Day4/Async-Prog-Assignment-Template/scripts/index.js
+++ Assignment/Day4/Async-Prog-Assignment-Template/scripts/index.js
@@ -1,18 +1,24 @@
 var moviesArr = JSON.parse(localStorage.getItem("movies")) || [];
 var movies = document.getElementById("movies");
-moviesArr.map(function (ele) {
-  var div = document.createElement("div");
-  var img = document.createElement("img");
-  var name = document.createElement("h2");
-  var date = document.createElement("h5");
-  var rating = document.createElement("h5");
-  img.setAttribute("src", ele.image);
-  name.innerText = ele.name;
-  date.innerText = "Release year : " + ele.date;
-  rating.innerText = "IMDB Rating : " + ele.rating;
-  div.append(img, name, date, rating);
-  movies.append(div);
-});
+
+display(moviesArr);
+
+function display(moviesArr) {
+  movies.innerHTML = "";
+  moviesArr.map(function (ele) {
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    var name = document.createElement("h2");
+    var date = document.createElement("h5");
+    var rating = document.createElement("h5");
+    img.setAttribute("src", ele.image);
+    name.innerText = ele.name;
+    date.innerText = "Release year : " + ele.date;
+    rating.innerText = "IMDB Rating : " + ele.rating;
+    div.append(img, name, date, rating);
+    movies.append(div);
+  });
+}
 var slideImg = document.getElementById("slideImg");
 setInterval(slideshow, 5000);
 var i = 0;
@@ -21,4 +27,15 @@ slideImg.src = moviesArr[0].image;
 function slideshow() {
   slideImg.src = moviesArr[i % n].image;
   i = i + 1;
+}
+document.getElementById("sort-lh").addEventListener("click", sortlh);
+document.getElementById("sort-hl").addEventListener("click", sorthl);
+
+function sortlh() {
+  moviesArr.sort((a, b) => a.rating - b.rating);
+  display(moviesArr);
+}
+function sorthl() {
+  moviesArr.sort((a, b) => b.rating - a.rating);
+  display(moviesArr);
 }
